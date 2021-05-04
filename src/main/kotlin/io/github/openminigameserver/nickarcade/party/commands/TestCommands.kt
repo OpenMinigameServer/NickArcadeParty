b036f113a435be807bef891f90f68f5e5d4e23f6
NickAcPT <32451103+NickAcPT@users.noreply.github.com>
2021-05-05 00:23:17 +0100

    Add all online players when creating a debug party

--- src/main/kotlin/io/github/openminigameserver/nickarcade/party/commands/TestCommands.kt
+++ src/main/kotlin/io/github/openminigameserver/nickarcade/party/commands/TestCommands.kt
@@ -4,19 +4,30 @@ import cloud.commandframework.annotations.CommandDescription
 import cloud.commandframework.annotations.CommandMethod
 import io.github.openminigameserver.hypixelapi.models.HypixelPackageRank
 import io.github.openminigameserver.nickarcade.core.data.sender.player.ArcadePlayer
+import io.github.openminigameserver.nickarcade.core.manager.getArcadeSender
 import io.github.openminigameserver.nickarcade.party.model.MemberRole
 import io.github.openminigameserver.nickarcade.party.model.PartySettings
 import io.github.openminigameserver.nickarcade.party.model.getCurrentParty
 import io.github.openminigameserver.nickarcade.party.model.getOrCreateParty
+import io.github.openminigameserver.nickarcade.plugin.extensions.command
 import io.github.openminigameserver.nickarcade.plugin.helper.commands.RequiredRank
+import org.bukkit.Bukkit
 
 object TestCommands {
     @RequiredRank(HypixelPackageRank.ADMIN)
     @CommandMethod("debugparty create")
     @CommandDescription("Create an empty party for debugging issues.")
-    fun createDebugParty(sender: ArcadePlayer) {
+    fun createDebugParty(sender: ArcadePlayer) = command(sender) {
         sender.getCurrentParty()?.disband()
-        sender.getOrCreateParty().settings.setPropertyAndNotify(sender, PartySettings::developerMode, true)
+        sender.getOrCreateParty().apply {
+            Bukkit.getOnlinePlayers().forEach {
+                if (it != sender.player) {
+                    addMember(it.getArcadeSender())
+                }
+            }
+            settings.setPropertyAndNotify(sender, PartySettings::developerMode, true)
+
+        }
     }
 
     @RequiredRank(HypixelPackageRank.ADMIN)
